8ba57ebff352f0e6da413afb454225df30314130
sfmacdonald <seanfmacdonald@icloud.com>
2024-03-20 14:57:20 -0700

    Refactor controllers js files

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,100 +1,92 @@
 const Thought = require('../models/Thought');
 
-// Controller function to get all thoughts
-const getAllThoughts = async (req, res) => {
-  try {
-    const thoughts = await Thought.find();
-    res.json(thoughts);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to get a single thought by ID
-const getThoughtById = async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    res.json(thought);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to create a new thought
-const createThought = async (req, res) => {
-  try {
-    const thought = await Thought.create(req.body);
-    res.status(201).json(thought);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Controller function to update a thought by ID
-const updateThought = async (req, res) => {
-  try {
-    const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true });
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    res.json(thought);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Controller function to delete a thought by ID
-const deleteThought = async (req, res) => {
-  try {
-    const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    res.json({ message: 'Thought deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to create a reaction for a thought
-const createReaction = async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    thought.reactions.push(req.body);
-    await thought.save();
-    res.status(201).json(thought);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Controller function to delete a reaction for a thought
-const deleteReaction = async (req, res) => {
-  try {
-    const thought = await Thought.findById(req.params.thoughtId);
-    if (!thought) {
-      return res.status(404).json({ message: 'Thought not found' });
-    }
-    thought.reactions = thought.reactions.filter(reaction => reaction._id.toString() !== req.params.reactionId);
-    await thought.save();
-    res.json(thought);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
 module.exports = {
-  getAllThoughts,
-  getThoughtById,
-  createThought,
-  updateThought,
-  deleteThought,
-  createReaction,
-  deleteReaction
+  // Controller function to get all thoughts
+  async getAllThoughts(req, res) {
+    try {
+      const thoughts = await Thought.find();
+      res.json(thoughts);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to get a single thought by ID
+  async getThoughtById(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.thoughtId);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to create a new thought
+  async createThought(req, res) {
+    try {
+      const thought = await Thought.create(req.body);
+      res.status(201).json(thought);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  // Controller function to update a thought by ID
+  async updateThought(req, res) {
+    try {
+      const thought = await Thought.findByIdAndUpdate(req.params.thoughtId, req.body, { new: true });
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json(thought);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  // Controller function to delete a thought by ID
+  async deleteThought(req, res) {
+    try {
+      const thought = await Thought.findByIdAndDelete(req.params.thoughtId);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      res.json({ message: 'Thought deleted' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to create a reaction for a thought
+  async createReaction(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.thoughtId);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      thought.reactions.push(req.body);
+      await thought.save();
+      res.status(201).json(thought);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  // Controller function to delete a reaction for a thought
+  async deleteReaction(req, res) {
+    try {
+      const thought = await Thought.findById(req.params.thoughtId);
+      if (!thought) {
+        return res.status(404).json({ message: 'Thought not found' });
+      }
+      thought.reactions = thought.reactions.filter(reaction => reaction._id.toString() !== req.params.reactionId);
+      await thought.save();
+      res.json(thought);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
 };
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,102 +1,94 @@
 const User = require('../models/User');
 
-// Controller function to get all users
-const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find();
-    res.json(users);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to get a single user by ID
-const getUserById = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to create a new user
-const createUser = async (req, res) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Controller function to update a user by ID
-const updateUser = async (req, res) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json(user);
-  } catch (err) {
-    res.status(400).json({ message: err.message });
-  }
-};
-
-// Controller function to delete a user by ID
-const deleteUser = async (req, res) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    res.json({ message: 'User deleted' });
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to add a friend to a user's friend list
-const addFriend = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    if (!user.friends.includes(req.params.friendId)) {
-      user.friends.push(req.params.friendId);
-      await user.save();
-    }
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
-// Controller function to remove a friend from a user's friend list
-const removeFriend = async (req, res) => {
-  try {
-    const user = await User.findById(req.params.userId);
-    if (!user) {
-      return res.status(404).json({ message: 'User not found' });
-    }
-    user.friends = user.friends.filter(friendId => friendId.toString() !== req.params.friendId);
-    await user.save();
-    res.json(user);
-  } catch (err) {
-    res.status(500).json({ message: err.message });
-  }
-};
-
 module.exports = {
-  getAllUsers,
-  getUserById,
-  createUser,
-  updateUser,
-  deleteUser,
-  addFriend,
-  removeFriend
-};
+  // Controller function to get all users
+  async getAllUsers(req, res) {
+    try {
+      const users = await User.find();
+      res.json(users);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to get a single user by ID
+  async getUserById(req, res) {
+    try {
+      const user = await User.findById(req.params.userId).populate('thoughts').populate('friends');
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to create a new user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.status(201).json(user);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  // Controller function to update a user by ID
+  async updateUser(req, res) {
+    try {
+      const user = await User.findByIdAndUpdate(req.params.userId, req.body, { new: true });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(400).json({ message: err.message });
+    }
+  },
+
+  // Controller function to delete a user by ID
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      res.json({ message: 'User deleted' });
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to add a friend to a user's friend list
+  async addFriend(req, res) {
+    try {
+      const user = await User.findById(req.params.userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      if (!user.friends.includes(req.params.friendId)) {
+        user.friends.push(req.params.friendId);
+        await user.save();
+      }
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+
+  // Controller function to remove a friend from a user's friend list
+  async removeFriend(req, res) {
+    try {
+      const user = await User.findById(req.params.userId);
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      user.friends = user.friends.filter(friendId => friendId.toString() !== req.params.friendId);
+      await user.save();
+      res.json(user);
+    } catch (err) {
+      res.status(500).json({ message: err.message });
+    }
+  },
+}
